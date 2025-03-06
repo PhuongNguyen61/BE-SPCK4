2e503e7cdb065473e1d16245263c6ab6c2f4bb46
TrungLe2003 <trungle.kqt@gmail.com>
2025-03-06 22:20:20 +0700

    Thêm phần admin k gửi được đơn

--- Be/controllers/mailController.js
+++ Be/controllers/mailController.js
@@ -22,15 +22,15 @@ const MailController = {
   sendMail: async (req, res) => {
     try {
       console.log("Request body:", req.body);
+      const role = req.user.role;
+      if (role === "ADMIN")
+        throw new Error("Vui lòng không sử dụng tài khoản Admin để gửi đơn");
       // Kiểm tra request body có dữ liệu không
       if (!req.body || Object.keys(req.body).length === 0) {
         throw new Error("Dữ liệu yêu cầu không hợp lệ.");
       }
       const { senderName, senderEmail, senderPhone, mailContent } = req.body;
       const { senderId, recipientId, carId } = req.query;
-      const role = req.user.role;
-      if (role === "ADMIN")
-        throw new Error("Vui lòng không sử dụng tài khoản Admin để gửi đơn");
       if (
         !senderName?.trim() ||
         senderName === "null" ||
