91ace81edaa83f1b22a70f89ee6ca6a790c277c2
TrungLe2003 <trungle.kqt@gmail.com>
2025-03-06 12:41:38 +0700

    Thêm phần đăng ký provider

--- Be/routes/v1/userRouter/index.js
+++ Be/routes/v1/userRouter/index.js
@@ -4,13 +4,41 @@ import UserMiddleware from "../../../middlewares/userMiddleware.js";
 import UserController from "../../../controllers/userController.js";
 const UserRouter = Router();
 
-UserRouter.post("/register", UserMiddleware.validateRegister, UserController.register); // Đăng ký - ALL
+UserRouter.post(
+  "/register",
+  UserMiddleware.validateRegister,
+  UserController.register
+); // Đăng ký - ALL
 UserRouter.post("/login", UserMiddleware.validateLogin, UserController.login); // Đăng nhập - ALL
-UserRouter.get("/countUsers", middlewares.verifyAccessToken, middlewares.validateAdmin, UserController.countUsers); // Đếm tất cả người dùng - ADMIN
-UserRouter.get("/", middlewares.verifyAccessToken, middlewares.validateAdmin, UserController.getListUser); // Lấy tất cả người dùng - ADMIN
-UserRouter.put("/modify/:id", middlewares.verifyAccessToken, middlewares.validateAdminOrAccountOwner, UserController.modifyUser); // Sửa thông tin người dùng - ADMIN || ACCOUNT OWNER
-UserRouter.delete("/deleteUserById/:id", middlewares.verifyAccessToken, middlewares.validateAdmin, UserController.deleteUserById); // Xóa người dùng - ADMIN
+UserRouter.get(
+  "/countUsers",
+  middlewares.verifyAccessToken,
+  middlewares.validateAdmin,
+  UserController.countUsers
+); // Đếm tất cả người dùng - ADMIN
+UserRouter.get(
+  "/",
+  middlewares.verifyAccessToken,
+  middlewares.validateAdmin,
+  UserController.getListUser
+); // Lấy tất cả người dùng - ADMIN
+UserRouter.put(
+  "/modify/:id",
+  middlewares.verifyAccessToken,
+  middlewares.validateAdminOrAccountOwner,
+  UserController.modifyUser
+); // Sửa thông tin người dùng - ADMIN || ACCOUNT OWNER
+UserRouter.delete(
+  "/deleteUserById/:id",
+  middlewares.verifyAccessToken,
+  middlewares.validateAdmin,
+  UserController.deleteUserById
+); // Xóa người dùng - ADMIN
 UserRouter.get("/:id", UserController.getUser); // Theo id
-
-
+UserRouter.post(
+  "/registerProvider/:id",
+  middlewares.verifyAccessToken,
+  middlewares.validateAdminOrAccountOwner,
+  UserController.registerProvider
+); //Đăng ký làm provider. Chờ admin duyệt
 export default UserRouter;
